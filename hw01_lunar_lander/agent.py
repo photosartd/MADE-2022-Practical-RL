5a5a8d3e4e133589ca6f8b2cb52144815cd5a843
photosartd <dmitry.trofimow2011@gmail.com>
2022-11-09 19:57:04 +0300

    hw1 added

--- hw01_lunar_lander/agent.py
+++ hw01_lunar_lander/agent.py
@@ -3,11 +3,21 @@ import numpy as np
 import os
 import torch
 
+from .train import QModel
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class Agent:
     def __init__(self):
-        self.model = torch.load(__file__[:-8] + "/agent.pkl")
+        self.model = QModel(8, 4).to(device)
+        self.model.load_state_dict(torch.load(__file__[:-8] + "/agent.pt"))
         
     def act(self, state):
-        return 0 # TODO
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        
+        self.model.eval()
+        with torch.no_grad():
+            action = np.argmax(self.model(state).cpu().data.numpy())
+        return action
 
